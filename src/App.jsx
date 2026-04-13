7a00d5085b89ee3ce516e4f0fe146eb11542780b
CodeWithRiyad <nurulafsarriyad06@gmail.com>
2026-04-13 16:10:00 +0600

    uploading files of layout practicing

---
+++ src/App.jsx
@@ -0,0 +1,116 @@
+
+
+function App() {
+
+
+  return (
+    <>
+    <div className="one">
+      <div className="two bg-yellow-100 flex">
+        <div className="three bg-red-100">
+           <div className="four bg-violet-100">15</div>
+             <div className="four bg-violet-200 flex">
+                <div className="five bg-blue-200">18</div>
+                <div className="five bg-blue-200">
+                  <div className="six bg-green-100">19</div>
+                    <div className="six bg-green-200">20</div>
+                   <div className="six bg-green-300">21</div>
+                </div>
+             </div>
+           <div className="four bg-violet-300">17</div>
+        </div>
+        <div className="three bg-red-200">8</div>
+        <div className="three bg-red-300">
+          <div className="seven bg-pink-100">22</div>
+          <div className="seven bg-pink-200 flex">
+            <div className="eight bg-cyan-200">23</div>
+            <div className="eight bg-cyan-300">24</div>
+            <div className="eight bg-cyan-400">25</div>
+          </div>
+          <div className="seven bg-pink-300">24</div>
+          <div className="seven bg-pink-400">25</div>
+        </div>
+        <div className="three bg-red-400">10</div>
+        <div className="three bg-red-500">
+           <div className="nine bg-lime-100">26</div>
+           <div className="nine flex">
+              <div className="ten bg-lime-400">27</div>
+              <div className="ten bg-lime-500">28</div>
+              <div className="ten bg-lime-600">29</div>
+              <div className="ten bg-lime-700">
+                <div className="h-[50%] flex">
+                  <div className="bg-amber-600">32</div>
+                  <div className="bg-amber-300">33</div>
+                </div>
+                <div className=" h-[50%] ">31</div>
+              </div>
+           </div>
+        </div>
+        <div className="three bg-red-600">12</div>
+        <div className="three bg-red-700">
+          <div className="eleven  flex">
+             <div className="w-[50%] bg-amber-500 ">
+               <div className="twelve bg-blue-300">35</div>
+               <div className="twelve bg-blue-400">36</div>
+             </div>
+             <div className="w-[50%] bg-amber-200">34</div>
+          </div>
+          <div className="eleven">33</div>
+        </div>
+        <div className="three bg-red-800">14</div>
+      </div>
+      <div className="two bg-yellow-200 flex">
+        <div className="w-[300px]">
+          <div className="thirteen bg-gray-300 flex">
+            <div className="fourteen bg-violet-200">39</div>
+            <div className="fourteen bg-violet-300">40</div>
+            <div className="fourteen bg-violet-400">
+              <div className="h-[50%] flex">
+                <div className="bg-amber-400 w-[50%]">45</div>
+                <div className="bg-amber-500 w-[50%]">46</div>
+              </div>
+              <div className="h-[50%]">44</div>
+            </div>
+            <div className="fourteen bg-violet-500">42</div>
+          </div>
+          <div className="thirteen bg-gray-400">38</div>
+        </div>
+        <div className="w-[250px]">38</div>
+        <div className="w-[450px]">
+           <div className="h-[65.5px]  bg-indigo-300 flex">
+              <div className="sixteen">49</div>
+              <div className="fifteen">
+                 <div className="h-[50%]  bg-teal-200 flex">
+                    <div className="w-1/2 h-full">
+                       <div className="h-[50%] bg-taupe-300">59</div>
+                        <div className="h-[50%] bg-taupe-500 ">60</div>
+                    </div>
+                    <div className="bg-amber-200 w-1/2">58</div>
+                 </div>
+                 <div className="h-[50%] bg-teal-100 flex">
+                  <div className="w-[10%] bg-teal-200">53</div>
+                  <div className="w-[10%] bg-teal-300">54</div>
+                  <div className="w-[10%] bg-teal-400">55</div>
+                  <div className="w-[10%] bg-teal-500">56</div>
+                  <div className="w-[10%] bg-teal-600">57</div>
+                  <div className="w-[10%] bg-teal-700">58</div>
+                  <div className="w-[10%] bg-teal-800">59</div>
+                  <div className="w-[10%] bg-teal-900">60</div>
+                  <div className="w-[10%] bg-green-300">60</div>
+                  <div className="w-[10%] bg-green-400">60</div>
+                 </div>
+              </div>
+           </div>
+              <div className="h-[65.5px]  bg-indigo-400">48</div>
+        </div>
+      </div>
+      <div className="two bg-yellow-300">3</div>
+      <div className="two bg-yellow-400">4</div>
+      <div className="two bg-yellow-500">5</div>
+      <div className="two bg-yellow-600">6 </div>
+    </div>
+    </>
+  )
+}
+
+export default App
