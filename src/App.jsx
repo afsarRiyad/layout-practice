ddd21cc427b8c5f70d16c100f87eb6ddedcc0826
CodeWithRiyad <nurulafsarriyad06@gmail.com>
2026-04-15 21:29:24 +0600

    uploading full layout practice (class H.W)

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,38 @@
 
-
 function App() {
 
 
   return (
     <>
-    <div className="one">
-      <div className="two bg-yellow-100 flex">
-        <div className="three bg-red-100">
-           <div className="four bg-violet-100">15</div>
-             <div className="four bg-violet-200 flex">
-                <div className="five bg-blue-200">18</div>
-                <div className="five bg-blue-200">
-                  <div className="six bg-green-100">19</div>
-                    <div className="six bg-green-200">20</div>
-                   <div className="six bg-green-300">21</div>
-                </div>
-             </div>
-           <div className="four bg-violet-300">17</div>
-        </div>
-        <div className="three bg-red-200">8</div>
-        <div className="three bg-red-300">
-          <div className="seven bg-pink-100">22</div>
-          <div className="seven bg-pink-200 flex">
-            <div className="eight bg-cyan-200">23</div>
-            <div className="eight bg-cyan-300">24</div>
-            <div className="eight bg-cyan-400">25</div>
+      <div className="one">
+        <div className="two bg-yellow-100 flex">
+          <div className="three bg-red-100">
+            <div className="four bg-violet-100">15</div>
+            <div className="four bg-violet-200 flex">
+              <div className="five bg-blue-200">18</div>
+              <div className="five bg-blue-200">
+                <div className="six bg-green-100">19</div>
+                <div className="six bg-green-200">20</div>
+                <div className="six bg-green-300">21</div>
+              </div>
+            </div>
+            <div className="four bg-violet-300">17</div>
           </div>
-          <div className="seven bg-pink-300">24</div>
-          <div className="seven bg-pink-400">25</div>
-        </div>
-        <div className="three bg-red-400">10</div>
-        <div className="three bg-red-500">
-           <div className="nine bg-lime-100">26</div>
-           <div className="nine flex">
+          <div className="three bg-red-200">8</div>
+          <div className="three bg-red-300">
+            <div className="seven bg-pink-100">22</div>
+            <div className="seven bg-pink-200 flex">
+              <div className="eight bg-cyan-200">23</div>
+              <div className="eight bg-cyan-300">24</div>
+              <div className="eight bg-cyan-400">25</div>
+            </div>
+            <div className="seven bg-pink-300">24</div>
+            <div className="seven bg-pink-400">25</div>
+          </div>
+          <div className="three bg-red-400">10</div>
+          <div className="three bg-red-500">
+            <div className="nine bg-lime-100">26</div>
+            <div className="nine flex">
               <div className="ten bg-lime-400">27</div>
               <div className="ten bg-lime-500">28</div>
               <div className="ten bg-lime-600">29</div>
@@ -44,50 +43,50 @@ function App() {
                 </div>
                 <div className=" h-[50%] ">31</div>
               </div>
-           </div>
-        </div>
-        <div className="three bg-red-600">12</div>
-        <div className="three bg-red-700">
-          <div className="eleven  flex">
-             <div className="w-[50%] bg-amber-500 ">
-               <div className="twelve bg-blue-300">35</div>
-               <div className="twelve bg-blue-400">36</div>
-             </div>
-             <div className="w-[50%] bg-amber-200">34</div>
-          </div>
-          <div className="eleven">33</div>
-        </div>
-        <div className="three bg-red-800">14</div>
-      </div>
-      <div className="two bg-yellow-200 flex">
-        <div className="w-[300px]">
-          <div className="thirteen bg-gray-300 flex">
-            <div className="fourteen bg-violet-200">39</div>
-            <div className="fourteen bg-violet-300">40</div>
-            <div className="fourteen bg-violet-400">
-              <div className="h-[50%] flex">
-                <div className="bg-amber-400 w-[50%]">45</div>
-                <div className="bg-amber-500 w-[50%]">46</div>
-              </div>
-              <div className="h-[50%]">44</div>
             </div>
-            <div className="fourteen bg-violet-500">42</div>
           </div>
-          <div className="thirteen bg-gray-400">38</div>
+          <div className="three bg-red-600">12</div>
+          <div className="three bg-red-700">
+            <div className="eleven  flex">
+              <div className="w-[50%] bg-amber-500 ">
+                <div className="twelve bg-blue-300">35</div>
+                <div className="twelve bg-blue-400">36</div>
+              </div>
+              <div className="w-[50%] bg-amber-200">34</div>
+            </div>
+            <div className="eleven">33</div>
+          </div>
+          <div className="three bg-red-800">14</div>
         </div>
-        <div className="w-[250px]">38</div>
-        <div className="w-[450px]">
-           <div className="h-[65.5px]  bg-indigo-300 flex">
+        <div className="two bg-yellow-200 flex">
+          <div className="w-[300px]">
+            <div className="thirteen bg-gray-300 flex">
+              <div className="fourteen bg-violet-200">39</div>
+              <div className="fourteen bg-violet-300">40</div>
+              <div className="fourteen bg-violet-400">
+                <div className="h-[50%] flex">
+                  <div className="bg-amber-400 w-[50%]">45</div>
+                  <div className="bg-amber-500 w-[50%]">46</div>
+                </div>
+                <div className="h-[50%]">44</div>
+              </div>
+              <div className="fourteen bg-violet-500">42</div>
+            </div>
+            <div className="thirteen bg-gray-400">38</div>
+          </div>
+          <div className="w-[250px]">38</div>
+          <div className="w-[450px]">
+            <div className="h-[65.5px]  bg-indigo-300 flex">
               <div className="sixteen">49</div>
               <div className="fifteen">
-                 <div className="h-[50%]  bg-teal-200 flex">
-                    <div className="w-1/2 h-full">
-                       <div className="h-[50%] bg-taupe-300">59</div>
-                        <div className="h-[50%] bg-taupe-500 ">60</div>
-                    </div>
-                    <div className="bg-amber-200 w-1/2">58</div>
-                 </div>
-                 <div className="h-[50%] bg-teal-100 flex">
+                <div className="h-[50%]  bg-teal-200 flex">
+                  <div className="w-1/2 h-full">
+                    <div className="h-[50%] bg-taupe-300">59</div>
+                    <div className="h-[50%] bg-taupe-500 ">60</div>
+                  </div>
+                  <div className="bg-amber-200 w-1/2">58</div>
+                </div>
+                <div className="h-[50%] bg-teal-100 flex">
                   <div className="w-[10%] bg-teal-200">53</div>
                   <div className="w-[10%] bg-teal-300">54</div>
                   <div className="w-[10%] bg-teal-400">55</div>
@@ -98,66 +97,135 @@ function App() {
                   <div className="w-[10%] bg-teal-900">60</div>
                   <div className="w-[10%] bg-green-300">61</div>
                   <div className="w-[10%] bg-green-400">62</div>
-                 </div>
+                </div>
               </div>
-           </div>
-              <div className="h-[69px]  bg-indigo-400">48</div>
-        </div>
-      </div>
-      <div className="two bg-yellow-300 flex">
-         <div className="w-[350px]  bg-rose-100">63</div>
-         <div className="w-[300px] bg-rose-200">
-             <div className="h-[30%] bg-amber-700">66</div>
-             <div className="h-[70%] bg-amber-500 flex">
-                 <div className="w-[50%] bg-rose-200">
-                    <div className="h-[50%] bg-rose-2000">69</div>
-                    <div className="h-[25%] bg-rose-300">70</div>
-                    <div className="h-[25%] bg-rose-400">71</div>
-                 </div>
-                 <div className="w-[50%] bg-rose-300">
-                    <div className="h-[25%] bg-rose-500">72</div>
-                    <div className="h-[25%] bg-rose-600">73</div>
-                    <div className="h-[50%] bg-rose-700">74</div>
-                 </div>
-             </div>
-         </div>
-         <div className="w-[350px] bg-rose-300">65</div>
-      </div>
-      <div className="two bg-yellow-400 flex">
-        <div className="w-[200px] bg-gray-50">75</div>
-        <div className="w-[200px] bg-gray-100">
-          <div className="h-[44px] bg-blue-50">80</div>
-          <div className="h-[44px] bg-blue-100 flex">
-            <div className="w-1/2 bg-green-50">83</div>
-            <div className="w-1/2 bg-green-100">84</div>
-            <div className="w-1/2 bg-green-200">85</div>
-            <div className="w-1/2 bg-green-300">86</div>
-            <div className="w-1/2 bg-green-400">87</div>
-            <div className="w-1/2 bg-green-500">88</div>
-            <div className="w-1/2 bg-green-600">89</div>
-          </div>
-          <div className="h-[46px] bg-blue-200">82</div>
-        </div>
-        <div className="w-[200px] bg-gray-200">77</div>
-        <div className="w-[200px] bg-gray-300">
-             <div className="h-[44px]">90</div>
-             <div className="h-[44px] flex">
-                <div className="w-1/2 bg-green-50">93</div>
-            <div className="w-1/2 bg-green-100">94</div>
-            <div className="w-1/2 bg-green-200">95</div>
-            <div className="w-1/2 bg-green-300">
-               <div className="bg-red-100 h-1/2">98</div>
-               <div className="bg-red-200 h-1/2">99</div>
             </div>
-            <div className="w-1/2 bg-green-400">97</div>
-             </div>
-             <div className="h-[46px] bg-gray-500">92</div>
+            <div className="h-[69px]  bg-indigo-400">48</div>
+          </div>
         </div>
-        <div className="w-[200px] bg-gray-400">79</div>
+        <div className="two bg-yellow-300 flex">
+          <div className="w-[350px]  bg-rose-100">63</div>
+          <div className="w-[300px] bg-rose-200">
+            <div className="h-[30%] bg-amber-700">66</div>
+            <div className="h-[70%] bg-amber-500 flex">
+              <div className="w-[50%] bg-rose-200">
+                <div className="h-[50%] bg-rose-2000">69</div>
+                <div className="h-[25%] bg-rose-300">70</div>
+                <div className="h-[25%] bg-rose-400">71</div>
+              </div>
+              <div className="w-[50%] bg-rose-300">
+                <div className="h-[25%] bg-rose-500">72</div>
+                <div className="h-[25%] bg-rose-600">73</div>
+                <div className="h-[50%] bg-rose-700">74</div>
+              </div>
+            </div>
+          </div>
+          <div className="w-[350px] bg-rose-300">65</div>
+        </div>
+        <div className="two bg-yellow-400 flex">
+          <div className="w-[200px] bg-gray-50">75</div>
+          <div className="w-[200px] bg-gray-100">
+            <div className="h-[44px] bg-blue-50">80</div>
+            <div className="h-[44px] bg-blue-100 flex">
+              <div className="w-1/2 bg-green-50">83</div>
+              <div className="w-1/2 bg-green-100">84</div>
+              <div className="w-1/2 bg-green-200">85</div>
+              <div className="w-1/2 bg-green-300">86</div>
+              <div className="w-1/2 bg-green-400">87</div>
+              <div className="w-1/2 bg-green-500">88</div>
+              <div className="w-1/2 bg-green-600">89</div>
+            </div>
+            <div className="h-[46px] bg-blue-200">82</div>
+          </div>
+          <div className="w-[200px] bg-gray-200">77</div>
+          <div className="w-[200px] bg-gray-300">
+            <div className="h-[44px]">90</div>
+            <div className="h-[44px] flex">
+              <div className="w-1/2 bg-green-50">93</div>
+              <div className="w-1/2 bg-green-100">94</div>
+              <div className="w-1/2 bg-green-200">95</div>
+              <div className="w-1/2 bg-green-300">
+                <div className="bg-red-100 h-1/2">98</div>
+                <div className="bg-red-200 h-1/2">99</div>
+              </div>
+              <div className="w-1/2 bg-green-400">97</div>
+            </div>
+            <div className="h-[46px] bg-gray-500">92</div>
+          </div>
+          <div className="w-[200px] bg-gray-400">79</div>
+        </div>
+        <div className="two bg-yellow-500 flex">
+            <div className="w-[500px]">
+                <div className="h-[50%] bg-pink-400">102</div>
+                <div className="h-[50%] flex">
+                   <div className="w-1/2 bg-blue-400">103</div>
+                   <div className="w-1/2 bg-yellow-500">
+                    <div className="h-[50%] bg-pink-100">108</div>
+                <div className="h-[50%] flex">
+                   <div className="w-1/2 bg-yellow-200">109</div>
+                    <div className="w-1/2 bg-yellow-300">110</div>
+                    <div className="w-1/2 bg-yellow-400">
+                         <div className="h-1/2 bg-red-300">113</div>
+                         <div className="h-1/2 bg-red-400">114</div> 
+                      </div>
+                    <div className="w-1/2 bg-yellow-500">112</div>
+                  </div>
+                 </div>
+                </div>
+            </div>
+            <div className="w-[500px] bg-amber-300">
+              <div className="h-[50%] flex">
+                <div className="w-1/2 bg-yellow-500">106</div>
+                <div className="w-1/2 bg-blue-100 ">107</div>
+              </div>
+              <div className="h-[50%] bg-pink-400">105</div>
+            </div>
+        </div>
+        <div className="two bg-yellow-600 flex">
+            <div className="w-1/2">
+              <div className="h-[31%] bg-green-50 ">117</div>
+              <div className="h-[31%] bg-green-500 ">118</div>
+              <div className="h-[40%] bg-green-200 flex ">
+                 <div className="w-[25%] bg-red-50">119</div>
+                 <div className="w-[25%] bg-red-300">120</div>
+                 <div className="w-[25%] bg-red-200">
+                    <div className="h-1/2 bg-taupe-200">123</div>
+                    <div className="h-1/2 bg-taupe-400 flex">
+                       <div className="w-[33%] bg-gray-50">24</div>
+                       <div className="w-[33%] bg-gray-400">25</div>
+                       <div className="w-[33%] bg-gray-500">26</div>
+                     </div>
+                 </div>
+                 <div className="w-[25%] bg-red-400">122</div>
+              </div>
+            </div>
+            <div className="w-1/2 bg-green-300">114</div>
+            <div className="w-1/2 bg-green-400">115</div>
+            <div className="w-1/2 ">
+               <div className="h-[33%] bg-indigo-400">1</div>  
+               <div className="h-[35%] bg-indigo-300 flex">
+                  <div className="w-1/2">
+                    <div className="bg-red-300 flex">
+                       <div className="w-1/2 bg-red-300">11</div>
+                       <div className="w-1/2 bg-red-700">12</div>
+                    </div>
+                    <div className="bg-red-400">9</div>
+                  </div>
+                  <div className="w-1/2">5</div>
+                  <div className="w-1/2">
+                    <div className="flex H-1/2">
+                        <div className="w-[33%] bg-pink-50">16</div>
+                        <div className="w-[33%] bg-pink-300">17</div>
+                        <div className="w-[33%] bg-pink-400">18</div>
+                    </div>
+                    <div className="bg-blue-700 h-1/2">15</div>
+                  </div>
+                  <div className="w-1/2 bg-green-500" >7</div>
+                </div>  
+               <div className="h-[33%] bg-indigo-200">3</div>  
+            </div>
+          </div>
       </div>
-      <div className="two bg-yellow-500">5</div>
-      <div className="two bg-yellow-600">6 </div>
-    </div>
     </>
   )
 }
